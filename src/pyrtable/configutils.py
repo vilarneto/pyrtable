73261f117cdd9c71af1f1c09c194743fbb9375f2
Vilar da Camara Neto <vilarneto@gmail.com>
2020-07-08 15:59:12 -0400

    Allow specifying configuration directories

--- src/pyrtable/configutils.py
+++ src/pyrtable/configutils.py
@@ -1,11 +1,39 @@
 import os
 import threading
+from typing import List, Optional
+
+
+_config_dirs: Optional[List[str]] = None
+_config_dirs_lock = threading.Lock()
+
+
+def get_config_dirs() -> List[str]:
+    global _config_dirs
+
+    import os
+
+    with _config_dirs_lock:
+        if _config_dirs is None:
+            _config_dirs = os.environ.get('PYRTABLE_CONFIG_DIRS')
+            if _config_dirs is not None:
+                _config_dirs = _config_dirs.split(':')
+
+        if _config_dirs is None:
+            home_dir = os.path.expanduser('~')
+            _config_dirs = [
+                os.path.join(os.path.abspath('.'), 'config'),
+                os.path.join(home_dir, '.config', 'airtable'),
+                '/etc/airtable']
+
+    general_config_dir = os.environ.get('CONFIG_DIR')
+    if general_config_dir is not None:
+        _config_dirs.insert(0, general_config_dir)
+
+    return _config_dirs
 
 
 def find_config_file(filename: str) -> str:
-    candidate_dirs = ['config', '/etc/airtable']
-
-    for candidate_dir in candidate_dirs:
+    for candidate_dir in get_config_dirs():
         if not os.path.isdir(candidate_dir):
             continue
         path = os.path.join(candidate_dir, filename)
@@ -63,4 +91,4 @@ def load_config_file(filename: str, loader=None, disable_caching=False):
         return contents
 
 
-__all__ = ['find_config_file', 'load_config_file']
+__all__ = ['get_config_dirs', 'find_config_file', 'load_config_file']
