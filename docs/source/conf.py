386124a0cc95da8ee4af9c64cbdc51a9adb06c86
Vilar da Camara Neto <vilarneto@gmail.com>
2021-02-11 09:38:11 -0400

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Vilar Fiuza da Camara Neto'
 author = 'Vilar Fiuza da Camara Neto'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.6'
+release = '0.7.7'
 
 
 # -- General configuration ---------------------------------------------------
